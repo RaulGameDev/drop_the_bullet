982268c13d011d23a58fa2542b76f2a684dddb3e
RaulGameDev <raul.avelar.alcantara@gmail.com>
2020-10-26 12:18:53 -0300

    proketo inicial

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float Speed;
+    public GameObject respawnPrefab;
+    public GameObject[] respawns;
+    //GameObject posicaoDoJogador;
+
+    void Start()
+    {
+        //posicaoDoJogador = GameObject.FindGameObjectsWithTag("Player");
+        //GameObject[] target = GameObject.FindGameObjectsWithTag("Human");
+        if (respawns == null)
+            respawns = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject respawn in respawns)
+        {
+           //Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
+           //Debug.Log(respawn.transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(posicaoDoJogador.gameObject != null);
+        {
+            //transform.position = Vector2 = MoveTowards(transform.position, posicaoDoJogador.position, velocidadeDoInimigo * Time.delataTime);
+            print(posicaoDoJogador);
+        }*/
+    }
+}
---
+++ Assets/Scripts/MovePato2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePato2 : MonoBehaviour {
+    
+    [SerializeField] float speed = default;
+    public float vida = 100;
+    public GameObject respawnPrefab;
+    public GameObject[] respawns;
+    void Start()
+    {
+        if (respawns == null)
+            respawns = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject respawn in respawns)
+        {
+           //Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
+           Debug.Log(respawn.transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+            if (Input.GetKey(KeyCode.R))
+            {
+                Debug.Log("R");
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-speed * Time.deltaTime,0,0), Space.World);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime,0,0), Space.World);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, speed * Time.deltaTime, 0), Space.World);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0), Space.World);
+            }
+    }
+}
